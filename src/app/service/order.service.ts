594ef279d0e5ff2e2251f24bdd07eec9b2e0de26
nrohland <nicolas.rohland@gmail.com>
2020-07-15 22:55:12 -0300

    Emoji Fixes

--- src/app/service/order.service.ts
+++ src/app/service/order.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Product, Extra, State } from './menu.service';
-import { HttpClient, HttpHeaders, HttpParams, HttpUrlEncodingCodec } from '@angular/common/http';
-import { API_URI , DATE_FORMAT, CLIENT_CART, USER} from '../app.constant';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpParams,
+  HttpUrlEncodingCodec,
+} from '@angular/common/http';
+import { API_URI, DATE_FORMAT, CLIENT_CART, USER } from '../app.constant';
 import { DatePipe } from '@angular/common';
 import { Socialusers } from './login.service';
 import { interval } from 'rxjs';
@@ -12,54 +17,63 @@ import { interval } from 'rxjs';
 export class OrderService {
   constructor(private httpClient: HttpClient) {}
 
-  parameterEncoding :ParameterEncoder
-  clientCart : Product[];
-  datePipe: DatePipe = new DatePipe("en-ES");
+  parameterEncoding: ParameterEncoder;
+  clientCart: Product[];
+  datePipe: DatePipe = new DatePipe('en-ES');
   headers = new HttpHeaders({
     'Content-Type': 'application/json',
   });
 
-  createOrder(orderRequest : OrderRequest){
+  createOrder(orderRequest: OrderRequest) {
     console.log(orderRequest);
-    
-    return this.httpClient.post(`${API_URI}order`, orderRequest, { headers: this.headers });
+
+    return this.httpClient.post(`${API_URI}order`, orderRequest, {
+      headers: this.headers,
+    });
   }
 
-  searchOrderList(status : string, dateFrom : Date , dateTo : Date, clientId : string){
-    let from = this.datePipe.transform(dateFrom, DATE_FORMAT)
-    let url = `${API_URI}order/search?dateFrom=${from}`
-    if(dateTo != null) {
-      let to = this.datePipe.transform(dateTo, DATE_FORMAT)
-      url += `&dateTo=${to}`
+  searchOrderList(
+    status: string,
+    dateFrom: Date,
+    dateTo: Date,
+    clientId: string
+  ) {
+    let from = this.datePipe.transform(dateFrom, DATE_FORMAT);
+    let url = `${API_URI}order/search?dateFrom=${from}`;
+    if (dateTo != null) {
+      let to = this.datePipe.transform(dateTo, DATE_FORMAT);
+      url += `&dateTo=${to}`;
     }
-    if(status != null){
-      url += `&status=${status}`
+    if (status != null) {
+      url += `&status=${status}`;
     }
-    if(clientId != null){
-      url += `&clientId=${clientId}`
+    if (clientId != null) {
+      url += `&clientId=${clientId}`;
     }
 
-    return this.httpClient.get(url, { headers: this.headers })
+    return this.httpClient.get(url, { headers: this.headers });
   }
 
   getStates() {
     return this.httpClient.get(`${API_URI}state`);
   }
 
-  getExtras(){
-    return this.httpClient.get(`${API_URI}extra`)
+  getExtras() {
+    return this.httpClient.get(`${API_URI}extra`);
   }
 
-  updateStatus(orderId:number, status : string, time : string){
+  updateStatus(orderId: number, status: string, time: string) {
     let request = new OrderRequest();
     request.status = status;
     request.orderId = orderId;
-    if( time != null) request.deliverTime = time
-    return this.httpClient.put(`${API_URI}order`,request, { headers: this.headers })
+    if (time != null) request.deliverTime = time;
+    return this.httpClient.put(`${API_URI}order`, request, {
+      headers: this.headers,
+    });
   }
 
-  saveClientCart(cart : Product[]){
-    localStorage.setItem(CLIENT_CART, JSON.stringify(cart))
+  saveClientCart(cart: Product[]) {
+    localStorage.setItem(CLIENT_CART, JSON.stringify(cart));
   }
 
   loadClientCart(): Product[] {
@@ -68,15 +82,17 @@ export class OrderService {
     }
   }
 
-  getOrderDetail(orderId : number){
-    return this.httpClient.get(`${API_URI}order?orderId=${orderId}`, { headers: this.headers })
+  getOrderDetail(orderId: number) {
+    return this.httpClient.get(`${API_URI}order?orderId=${orderId}`, {
+      headers: this.headers,
+    });
   }
 
-  getKitchenStatus(){
-    return this.httpClient.get(`${API_URI}kitchen`, { headers: this.headers })
+  getKitchenStatus() {
+    return this.httpClient.get(`${API_URI}kitchen`, { headers: this.headers });
   }
 
-  cancelOrder(orderId:number, status : string){
+  cancelOrder(orderId: number, status: string) {
     let request = new OrderRequest();
     request.status = status;
     request.orderId = orderId;
@@ -84,39 +100,45 @@ export class OrderService {
       headers: this.headers,
       body: request,
     };
-    return this.httpClient.delete(`${API_URI}order`, options)
+    return this.httpClient.delete(`${API_URI}order`, options);
   }
 
-  cartClientMessageUrl(order:Order, enviado : boolean){
+  cartClientMessageUrl(order: Order, enviado: boolean) {
     this.parameterEncoding = new ParameterEncoder();
-    let msg = `Hola Soy ${order.client.name}! ${enviado ? `Realice un pedido desde vanburga.com con id N°${order.id}:\n` : 'No pude realizar el pedido por la pagina, les envio lo que queria pedir:\n'}`
-    order.products.forEach(p => {
-      if(p.category.id == 1){
-        msg += ':hamburger:'
-      }else{
-        msg += ':beer::fries:'
+    let msg = `Hola Soy ${order.client.name}! ${
+      enviado
+        ? `Realice un pedido desde vanburga.com con id N°${order.id}:\n`
+        : 'No pude realizar el pedido por la pagina, les envío lo que quería pedir:\n'
+    }`;
+    order.products.forEach((p) => {
+      if (p.category.id == 1) {
+        msg += '🍔';
+      } else {
+        msg += '🍺🍟';
       }
-      msg += ` ${p.name}\n`
-      p.extras?.forEach(e=>{
-        msg += `\t*${e.name}* X${e.quantity}\n`
-      })
-    })
-    if(order.delivery){
-      msg += ':red_car:Nos solicitaste el envio a la siguiente direccion:\n'
-      msg += `calle: ${order.client.address.street} ${order.client.address.doorNumber} \n`
-      msg += `entre calles: ${ order.client.address.reference } en ${ order.client.address.state.state }\n`
-      if(order.client.address.floor != null) msg += `Piso: ${ order.client.address.floor }\n`
-      if(order.client.address.door != null) msg += `Depto: ${ order.client.address.door }\n`
+      msg += ` ${p.name}\n`;
+      p.extras?.forEach((e) => {
+        msg += `\t*${e.name}* X${e.quantity}\n`;
+      });
+    });
+    if (order.delivery) {
+      msg += '🚗 Nos solicitaste el envio a la siguiente direccion:\n';
+      msg += `calle: ${order.client.address.street} ${order.client.address.doorNumber} \n`;
+      msg += `entre calles: ${order.client.address.reference} en ${order.client.address.state.state}\n`;
+      if (order.client.address.floor != null)
+        msg += `Piso: ${order.client.address.floor}\n`;
+      if (order.client.address.door != null)
+        msg += `Depto: ${order.client.address.door}\n`;
     }
-    if(order.comments != null) msg += `:notepsd_spiral: Comentarios adicionales: ${ order.comments }\n`
-    msg += 'Saludos!'
+    if (order.comments != null)
+      msg += `📝Comentarios adicionales: ${order.comments}\n`;
+    msg += 'Saludos!';
 
-    return this.parameterEncoding.encodeValue(msg)
+    return this.parameterEncoding.encodeValue(msg);
   }
-
 }
 
-export class ParameterEncoder extends HttpUrlEncodingCodec{
+export class ParameterEncoder extends HttpUrlEncodingCodec {
   encodeKey(key: string): string {
     return encodeURIComponent(key);
   }
@@ -134,8 +156,8 @@ export class ParameterEncoder extends HttpUrlEncodingCodec{
 //////////////////////////////////////////////////////////////
 //    OBJETOS DE METODOS A LA API
 //////////////////////////////////////////////////////////////
-export interface KitchenStatus{
-  chips: number;   
+export interface KitchenStatus {
+  chips: number;
   grilledHamburger: number;
   simpleCheddar: number;
   doubleCheddar: number;
@@ -146,7 +168,7 @@ export interface KitchenStatus{
   noCheese: number;
   orderCount: number;
   productCount: number;
-  orders ?: Order[];
+  orders?: Order[];
 }
 
 export interface OrderList {
@@ -157,13 +179,13 @@ export interface OrderList {
 }
 
 export interface OrderResponse {
-  message?:     string;
-  code?:        number;
-  status?:      number;
-  address?:     Address;
-  order?:       Order;
-  products?:     Product[];
-  orders?:       Order[];
+  message?: string;
+  code?: number;
+  status?: number;
+  address?: Address;
+  order?: Order;
+  products?: Product[];
+  orders?: Order[];
 }
 
 export class OrderRequest {
@@ -171,8 +193,8 @@ export class OrderRequest {
   comment?: string;
   products?: Product[];
   delivery?: boolean;
-  orderId?:   number;
-  status?:    string;
+  orderId?: number;
+  status?: string;
   paymentType?: string;
   deliverTime?: string;
 }
@@ -188,11 +210,11 @@ export class Order {
   status?: string;
   createDate?: Date;
   amount?: number;
-  products? : Product[];
+  products?: Product[];
   selected?: boolean;
   delivery?: boolean;
-  totalRawMaterial? : number;
-  confirmed? : boolean;
+  totalRawMaterial?: number;
+  confirmed?: boolean;
   paymentType?: string;
   deliverTime?: string;
 }
@@ -208,10 +230,10 @@ export class Client {
 export class Address {
   street?: string;
   doorNumber?: string;
-  zipCode?:    string;
-  state?:      State;
-  floor?:     string;
-  door?:      string;
+  zipCode?: string;
+  state?: State;
+  floor?: string;
+  door?: string;
   reference?: string;
 }
 
